dacfd875f44e9963caf76cd21ea096f806292df1
Cristina Carceller Segarra <cristina@cristinas-air.home>
2023-05-14 19:32:11 +0100

    finish style

--- index.js
+++ index.js
@@ -13,12 +13,28 @@ const success = async ({ coords }) => {
 const setWeather = (list) => {
 	console.log({ list });
 	const html = list.map((item) => {
-		return `<div class="item">
-        <p>${new Date(item.dt * 1000).toLocaleString()}</p>
-        <p>${Math.round(item.main.temp - 273.15)}&#8451</p>
-        <p>${item.weather[0].description}</p>
+		const start = `<div class="item">
+		<p> ${new Date(item.dt * 1000).toLocaleString()}</p>
+		<p> ${Math.round(item.main.temp - 273.15)}&#8451</p>
+		<p> ${item.weather[0].description}</p>`;
 
-        </div>`;
+		const end = `</div>`;
+
+		let svg = "";
+
+		if (item.weather[0].description.includes("cloud")) {
+			svg = `<div class="icon"><svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 336c0 79.5 64.5 144 144 144H512c70.7 0 128-57.3 128-128c0-61.9-44-113.6-102.4-125.4c4.1-10.7 6.4-22.4 6.4-34.6c0-53-43-96-96-96c-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32C167.6 32 96 103.6 96 192c0 2.7 .1 5.4 .2 8.1C40.2 219.8 0 273.2 0 336z"/></svg></div>`;
+		}
+		if (item.weather[0].description.includes("rain")) {
+			svg = `<div class="icon"><svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M96 320c-53 0-96-43-96-96c0-42.5 27.6-78.6 65.9-91.2C64.7 126.1 64 119.1 64 112C64 50.1 114.1 0 176 0c43.1 0 80.5 24.3 99.2 60c14.7-17.1 36.5-28 60.8-28c44.2 0 80 35.8 80 80c0 5.5-.6 10.8-1.6 16c.5 0 1.1 0 1.6 0c53 0 96 43 96 96s-43 96-96 96H96zM81.5 353.9c12.2 5.2 17.8 19.3 12.6 31.5l-48 112c-5.2 12.2-19.3 17.8-31.5 12.6S-3.3 490.7 1.9 478.5l48-112c5.2-12.2 19.3-17.8 31.5-12.6zm120 0c12.2 5.2 17.8 19.3 12.6 31.5l-48 112c-5.2 12.2-19.3 17.8-31.5 12.6s-17.8-19.3-12.6-31.5l48-112c5.2-12.2 19.3-17.8 31.5-12.6zm244.6 31.5l-48 112c-5.2 12.2-19.3 17.8-31.5 12.6s-17.8-19.3-12.6-31.5l48-112c5.2-12.2 19.3-17.8 31.5-12.6s17.8 19.3 12.6 31.5zM313.5 353.9c12.2 5.2 17.8 19.3 12.6 31.5l-48 112c-5.2 12.2-19.3 17.8-31.5 12.6s-17.8-19.3-12.6-31.5l48-112c5.2-12.2 19.3-17.8 31.5-12.6z"/></svg></div>`;
+		}
+		if (item.weather[0].description.includes("clear")) {
+			svg = `<div class="icon"><svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z"/></svg></div>`;
+		}
+
+		const result = start + end;
+
+		return start + svg + end;
 	});
 	root.innerHTML = html.join(" ");
 };
@@ -35,14 +51,10 @@ const config = {
 
 navigator.geolocation.getCurrentPosition(success, error, config);
 
-// let cityName = "";
-
 const getCityWeather = async (cityName) => {
 	const data = await axios.get(
 		`https://api.openweathermap.org/data/2.5/forecast?q=${cityName},GB&appid=385ba9d34e4cd03058a4a2ef1618c550`
 	);
-
-	console.log({ cityData: data });
 
 	setWeather(data.data.list);
 };
@@ -50,7 +62,6 @@ const getCityWeather = async (cityName) => {
 const search = document
 	.getElementById("search")
 	.addEventListener("change", (e) => {
-		console.log({ e });
 		getCityWeather(e.target.value);
 
 		e.target.value = "";
