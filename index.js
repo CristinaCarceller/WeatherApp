c6b458928695296bf8e557516f36e36d460c4ec5
Cristina Carceller Segarra <cristina@cristinas-air.home>
2023-05-14 17:32:55 +0100

    final js

--- index.js
+++ index.js
@@ -1,25 +1,57 @@
-// 1. Use axios to get weather data from the API I showed at the start of the lesson
-// 2. Wrap the axios call in an async function
-// 3. Use await before the axios call
-// 4. Destructure the response to get the { data } part of the response
-// 5. Console.log the weather data (this will show you completed the above)
+const root = document.getElementById("root");
 
-const apiURL =
-	"https://api.openweathermap.org/data/2.5/forecast?lat=52&lon=-2&appid=385ba9d34e4cd03058a4a2ef1618c550";
+const success = async ({ coords }) => {
+	const { latitude, longitude } = coords;
 
-const getData = async () => {
-	const { data } = await axios.get(apiURL);
-	console.log(data.list);
+	const { data } = await axios.get(
+		`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=385ba9d34e4cd03058a4a2ef1618c550`
+	);
+	console.log({ data });
+	setWeather(data.list);
+};
 
-	const html = data.list.map((item) => {
+const setWeather = (list) => {
+	console.log({ list });
+	const html = list.map((item) => {
 		return `<div class="item">
         <p>${new Date(item.dt * 1000).toLocaleString()}</p>
         <p>${Math.round(item.main.temp - 273.15)}&#8451</p>
         <p>${item.weather[0].description}</p>
 
-    </div>`;
+        </div>`;
 	});
-	document.getElementById("root").innerHTML = html.join(" ");
+	root.innerHTML = html.join(" ");
 };
 
-getData();
+const error = (error) => {
+	console.log(error);
+};
+
+const config = {
+	enableHighAccuracy: true,
+	maximumAge: 0,
+	timeout: 500,
+};
+
+navigator.geolocation.getCurrentPosition(success, error, config);
+
+// let cityName = "";
+
+const getCityWeather = async (cityName) => {
+	const data = await axios.get(
+		`https://api.openweathermap.org/data/2.5/forecast?q=${cityName},GB&appid=385ba9d34e4cd03058a4a2ef1618c550`
+	);
+
+	console.log({ cityData: data });
+
+	setWeather(data.data.list);
+};
+
+const search = document
+	.getElementById("search")
+	.addEventListener("change", (e) => {
+		console.log({ e });
+		getCityWeather(e.target.value);
+
+		e.target.value = "";
+	});
